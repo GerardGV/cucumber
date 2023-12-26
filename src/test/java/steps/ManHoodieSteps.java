dd47ceb073e92bc86020c08d3d39102664be73f4
gerard <1605947@uab.cat>
2023-12-26 12:13:37 +0100

    first commit

---
+++ src/test/java/steps/ManHoodieSteps.java
@@ -0,0 +1,29 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ManHoodieSteps {
+    @Given("the user is in the index page")
+    public void userInIndexPage(){
+
+    }
+    @When("the user clicks man options")
+    public void userClickManOptions(){
+
+    }
+    @When("the user enters hoodie in the search bar")
+    public void userEntersHoodie(){
+
+    }
+    @When("the user clicks the search button")
+    public void userClicksSearch(){
+
+    }
+
+    @Then("the hoodie list appears")
+    public void hoodieListAppears(){
+
+    }
+}
